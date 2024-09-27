7b612427d6aa84f4f403fed8c10e5f3ec8d568fb
Camille <54495608+neo936@users.noreply.github.com>
2024-09-27 10:49:11 +0200

    refacto GildedRose with new branch

--- Java/src/main/java/com/gildedrose/GildedRoseConstants.java
+++ Java/src/main/java/com/gildedrose/GildedRoseConstants.java
@@ -1,13 +1,9 @@
 package com.gildedrose;
 
 public class GildedRoseConstants {
-
     public final static  String AGED_BRIE = "Aged Brie";
-
     public final static  String SULFURAS = "Sulfuras, Hand of Ragnaros";
-
     public final static  String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-
     public final static  String CONJURED = "Conjured Mana Cake";
     public final static int MAX_QUALITY_ITEM = 50;
 
